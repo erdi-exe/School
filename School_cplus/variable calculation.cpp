c30d4e28247d4d016708c4f3284edc0b51263667
Erdi <zzcluboy@gmail.com>
2026-09-20 20:50:27 +0200

    Add files via upload

---
+++ School_cplus/variable calculation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;    
+
+int main () {
+    int x = 10;
+    cout << x << endl;
+    return 0;
+    int y = 20;
+    cout << y << endl;
+    cout << x + y << endl;
+    return 0;
+}
