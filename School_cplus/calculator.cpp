51433415615b3cc7e0ecc3841203884e686188d9
Erdi <zzcluboy@gmail.com>
2026-09-21 21:48:54 +0200

    Add files via upload
    basic calculator with the sum of 2 numbers

---
+++ School_cplus/calculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout <<"Enter first number: "<<endl;
+    cin>>a;
+    cout<<"Enter your second number: "<<endl;
+    cin>>b;
+    cout<<"sum of two numbers is: " << a+b << endl;
+    return 0;
+}
